11b5a6c1e4cca96cbc3eff8ed72884712bdd680a
kishor327 <kishor.subramaniyam@tokiomarinekiln.com>
2025-06-09 09:35:28 +0100

    update - theme change

--- R/create-plot.R
+++ R/create-plot.R
@@ -14,8 +14,8 @@ city = "Sydney"
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point()
-
+  ggplot(aes(x = Temp9am, y = Humidity9am),theme_minimal()) + 
+  geom_point(colour = "red")
+scatter_plot
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
